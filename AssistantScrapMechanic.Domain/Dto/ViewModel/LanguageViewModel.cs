cc053fcc072d9044125c37870f9f436ce47cfdbf
Kurt Lourens <hi@kurtlourens.com>
2020-12-29 15:35:22 +0200

    get language keys from server

---
+++ AssistantScrapMechanic.Domain/Dto/ViewModel/LanguageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AssistantScrapMechanic.Domain.Dto.ViewModel
+{
+    public class LanguageViewModel
+    {
+        public Guid Guid { get; set; }
+        public string Name { get; set; }
+        public string LanguageCode { get; set; }
+        public string CountryCode { get; set; }
+        public int SortOrder { get; set; }
+        public bool IsVisible { get; set; }
+    }
+}
